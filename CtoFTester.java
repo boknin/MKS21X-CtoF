8f54386a322724c2281763132385abbe7c09a211
Leo Smulansky <smulanskyleo@gmail.com>
2021-09-23 18:40:43 -0400

    added skeletons of functions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,10 @@
-public class CtoFTester {}
+public class CtoFTester {
+  public static double celsiusToFahrenheit() {
+    return 0.0;
+  }
+
+  public static double fahrenheitToCelsius() {
+    return 0.0;
+  }
+
+}
